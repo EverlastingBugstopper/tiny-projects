d9933c2ca4c1afaedef54fc4015e146391cd99ee
Avery Harnish <harnisha@lafayette.edu>
2016-09-15 17:02:34 -0400

    main.py now enters the associated function based off of the menu selection

--- calc3_scripts/vectors/main.py
+++ calc3_scripts/vectors/main.py
@@ -5,7 +5,18 @@ from inputmenu import Menu
 def main():
 	m1 = Menu("Vector Functions", ["Dot Product", "Cross Product"])
 	m1.get_input()
-	m1.get_result()
+	functionName = m1.get_result()
+	if functionName == "Dot Product":
+		success(functionName)
+		dotproduct.main()
+	elif functionName == "Cross Product":
+		success(functionName)
+		crossproduct.main()
+	else:
+		print("Menu returned unavailable function.")
+
+def success(name):
+	print("Entering " + name)
 
 
 if __name__=="__main__":
